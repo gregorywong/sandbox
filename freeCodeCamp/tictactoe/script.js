d49a9222854fe115fa1a73f6e83f85103378d543
Greg Wong <Gregory.LT.Wong@gmail.com>
2017-06-22 16:22:29 +0800

    Setting up tests

---
+++ freeCodeCamp/tictactoe/script.js
@@ -0,0 +1,12 @@
+
+
+runTests();
+
+/* Test suite */
+function runTests() {
+
+  var temp = 0;
+
+  console.log('Running test ' + temp + ' of ' + temp + '.');
+  console.log('Finished running ' + temp + ' tests with a total of ' + temp + ' errors.');
+};
