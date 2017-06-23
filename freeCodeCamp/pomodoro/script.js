ddaa1b0957d13945214fb173aa64b113d54125ef
Greg Wong <Gregory.LT.Wong@gmail.com>
2017-06-23 23:21:22 +0800

    Working pomodoro timer. TODO: add visual alarm, find better audio

--- freeCodeCamp/pomodoro/script.js
+++ freeCodeCamp/pomodoro/script.js
@@ -3,10 +3,10 @@ var CountDown = function(sec, tickCallback, timesUpCallback) {
   var setSeconds = sec;
   validateSetSeconds(); // change it to a suitable value if necessary
   var seconds = setSeconds;
-  console.log(seconds);
   var timerID = null;
   var tickCallback = tickCallback;
   var timesUpCallback = timesUpCallback;
+  var alarmOn = false;
   const INTERVAL = 1000;
 
   this.set = function(sec) {
@@ -29,9 +29,23 @@ var CountDown = function(sec, tickCallback, timesUpCallback) {
 
   this.stop = function() {
     stop();
+    alarmOn = false;
+  };
+
+  this.isRunning = function() {
+    return timerID != null;
+  };
+
+  this.isAlarmOn = function() {
+    return alarmOn;
+  };
+
+  this.getSetSeconds = function() {
+    return setSeconds;
   };
 
   function validateSetSeconds() {
+    setSeconds = Math.floor(setSeconds); // in case a float was passed in
     if (setSeconds < 0) {
       setSeconds = 0;
     }
@@ -45,6 +59,7 @@ var CountDown = function(sec, tickCallback, timesUpCallback) {
     if (seconds <= 0) {
       seconds = 0; // don't display negative values
       timesUpCallback();
+      alarmOn = true;
       stop();
     }
     tickCallback(seconds);
@@ -58,30 +73,96 @@ var CountDown = function(sec, tickCallback, timesUpCallback) {
   }
 };
 
-function getTimeFormat(sec) {
+function getMinutesAndSeconds(sec) {
   // return seconds in MM:SS format
   var minutes = Math.floor(sec / 60);
   minutes = minutes < 10 ? '0'+ minutes : minutes;
   var seconds = sec % 60;
   seconds = seconds < 10 ? '0'+ seconds : seconds;
 
-  return minutes + ":" + seconds;
+  return [minutes, seconds];
 }
 
-// for testing
-window.onload = function(){
-  var testSec = 5;
-  document.getElementById('timer').innerHTML = getTimeFormat(testSec);
+function toSeconds(minutes, seconds) {
+  return minutes * 60 + seconds;
+}
 
-  var myCountDown = new CountDown(testSec, updateDisplay, timesUp);
-  myCountDown.start();
-
-  function updateDisplay(sec) {
-    document.getElementById('timer').innerHTML = getTimeFormat(sec);
+function getSuitableMinutes(min) {
+  if (min < 0) {
+    return 0;
   }
-
-  function timesUp() {
-    document.getElementById('timesup').innerHTML = "Time's up!"
+  else if (min > 99) {
+    return 99;
   }
+  return min;
+}
 
-};
+function getSuitableSeconds(sec) {
+  if (sec < 0) {
+    return 0;
+  }
+  else if (sec > 59) {
+    return 59;
+  }
+  return sec;
+
+}
+
+$(document).ready(function() {
+
+  var alarm = document.getElementById("alarm");
+
+  var updateDisplay = function(sec) {
+    console.log(sec);
+    var time = getMinutesAndSeconds(sec);
+    console.log(time);
+    $("#minutes").val(time[0]);
+    $("#seconds").val(time[1]);
+  };
+
+  var timesUp = function() {
+    alarm.play();
+  // TODO: what happens when alarm goes off?
+  };
+
+  var myCountDown = new CountDown(toSeconds(25,0), updateDisplay, timesUp);
+
+  $(".time-input").focusin(function(event) {
+    alarm.pause(); // if it's not playing, this won't do anything
+    myCountDown.stop();
+  });
+
+  $(".time-input").focusout(function(event) {
+    var minutes = parseInt($("#minutes").val());
+    var seconds = parseInt($("#seconds").val());
+
+    if (!minutes) {
+      minutes = 0;
+    }
+    if (!seconds) {
+      seconds = 0;
+    }
+
+    minutes = getSuitableMinutes(minutes);
+    seconds = getSuitableSeconds(seconds);
+
+    var sec = minutes * 60 + seconds;
+    updateDisplay(sec);
+    myCountDown.set(sec);
+  });
+
+  $("#start-stop-button").click(function(event) {
+    if (myCountDown.isAlarmOn()) {
+      alarm.pause();
+      myCountDown.stop();
+      updateDisplay(myCountDown.getSetSeconds());
+    }
+    else if (myCountDown.isRunning()) {
+      myCountDown.stop();
+    }
+    else {
+      myCountDown.start();
+    }
+  });
+
+});
