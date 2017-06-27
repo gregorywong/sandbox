b42e6aa309c29e36fdeee9660d8ff959d62c8359
Greg Wong <Gregory.LT.Wong@gmail.com>
2017-06-27 16:27:13 +0800

    removed unnecessary comments

--- freeCodeCamp/tictactoe/board.js
+++ freeCodeCamp/tictactoe/board.js
@@ -1,7 +1,3 @@
-// User Story: I can play a game of Tic Tac Toe with the computer.
-// User Story: My game will reset as soon as it's over so I can play again.
-// User Story: I can choose whether I want to play as X or O.
-
 var Board = function() {
   const ROWS = 3;
   const COLS = 3;
