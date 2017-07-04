afbcb446edde99ba6fd21909b787798776e2cc8c
Greg Wong <Gregory.LT.Wong@gmail.com>
2017-07-04 18:16:06 +0800

    Finished basic game flow; need to complete TODOs

--- freeCodeCamp/simon/script.js
+++ freeCodeCamp/simon/script.js
@@ -1,10 +1,9 @@
 var strictMode = false;
-// TODO:
-// var gameStarted = false;
-var gameStarted = true;
-const FLASH_TIME = 500; // milliseconds
+const FLASH_DURATION = 500; // milliseconds
+var count = 0;
 
 $(document).ready(function() {
+
 
   // strict mode listener
   $("#strict-mode").change(function() {
@@ -13,33 +12,45 @@ $(document).ready(function() {
 
   // start button listener
   $("#start-button").click(function(event) {
+    count = 0;
+    $("#count").val("--");
+
+    // TODO: start sound
     flashToggle(); // show an animation
-    gameStarted = true;
     setTimeout(function() {
       flashToggle(); // turn off flashing
-      // all color buttons are set to unclickable at first, so activate them
-      toggleClickableButtons();
+      count++;
     }, 2000); // stop the animation
 
-    // TODO: counter
-    // TODO: start sound
+    roundStart(); // display flash animation and "progress to" to level 1
   });
 
   // color button listeners
   $(".color-button").click(function(event) {
-    if (gameStarted) {
-      var color = $(this).attr("id");
+    var color = $(this).attr("id");
 
-      toggleClickableButtons();
-      flash(color,toggleClickableButtons);
+    disableClickableButtons();
+    flash(color, function() {
+      // TODO: check if correct
+      // if correct and already entered everything
+      // roundPassed();
+      // if incorrect
+      // incorrectReaction();
+    });
 
-      // TODO: play sound
-    }
   });
 });
 
 function setCount(num) {
-  $("#count").val(num);
+  if (isNaN(num)) {
+    console.error('count should be a number')
+  }
+  else {
+    if (num < 9) { // only 1 digit
+      num = '0' + num;
+    }
+    $("#count").val(num);
+  }
 }
 
 function flashToggle() {
@@ -49,16 +60,97 @@ function flashToggle() {
   $("#yellow").toggleClass('flash-yellow');
 }
 
-function toggleClickableButtons() {
-  $(".color-button").toggleClass('unclickable');
+function enableClickableButtons() {
+  $(".color-button").removeClass('unclickable');
+}
+
+function disableClickableButtons() {
+  $(".color-button").addClass('unclickable');
 }
 
 function flash(color, callback) {
+  // TODO: play corresponding sound
   $('#'+color).addClass(color+'-flash');
   setTimeout(function() {
     $('#'+color).removeClass(color+'-flash');
     if (callback) {
       callback();
     }
-  }, FLASH_TIME);
+  }, FLASH_DURATION);
+}
+
+function toggleCountBoxColor() {
+  $(".count-box").toggleClass('text-red');
+}
+
+function roundStart() {
+  setTimeout(function() { // wait one second
+
+    // TODO: play sequence
+
+    // all color buttons are set to unclickable at first, so activate them
+    enableClickableButtons();
+
+  }, 1000);
+}
+
+function roundPassed() {
+  disableClickableButtons();
+
+  // TODO: play round passed sound
+
+  flashToggle(); // show an animation
+  setTimeout(function() {
+    count++;
+    if (count > 20) {
+      // TODO: play victory sound
+
+      setTimeout(function() {
+        flashToggle(); // turn off flashing
+
+        // game effectively resets at this point
+        count = 0;
+        $("#count").val("--");
+      }, 2000);
+    }
+    else {
+      flashToggle(); // turn off flashing
+      setCount(count);
+      toggleCountBoxColor();
+      setTimeout(function() {
+        toggleCountBoxColor();
+      }, 1000);
+
+      // TODO: randomly generate sequence
+
+      roundStart();
+    }
+
+  }, 2000); // stop the animation
+}
+
+function incorrectReaction() {
+  disableClickableButtons();
+
+  $("#green").addClass('blink-green');
+  $("#red").addClass('blink-red');
+  $("#blue").addClass('blink-blue');
+  $("#yellow").addClass('blink-yellow');
+
+  // TODO: play buzzer sound
+
+  setTimeout(function() {
+    $("#green").removeClass('blink-green');
+    $("#red").removeClass('blink-red');
+    $("#blue").removeClass('blink-blue');
+    $("#yellow").removeClass('blink-yellow');
+
+    if (strictMode) {
+      count = 0;
+      $("#count").val("--");
+    }
+    else {
+      roundStart();
+    }
+  }, 1500);
 }
